4f9ae4dcb46e793ec80e581fea18deac3798da50
Adam Grayson <asgrayson90@gmail.com>
2025-07-24 12:40:32 -0400

    DnD stuff

--- src/components/battle.tsx
+++ src/components/battle.tsx
@@ -1,35 +1,89 @@
 import { useBattle } from '@/hooks';
+import { DragDropContext, Draggable, Droppable, DropResult } from '@hello-pangea/dnd';
 
 export const Battle = () => {
 	const { battle, player, enemies, handleCastSpell, combatLog } = useBattle();
 
+	const handleDragEnd = (result: DropResult) => {
+		if (!result.destination) {
+			return;
+		}
+
+		const spellId = player?.spells.find((s) => s.id === result.draggableId)?.spellId;
+
+		if (!spellId) {
+			return;
+		}
+
+		handleCastSpell({
+			casterId: player?.id ?? '',
+			targetId: result.destination.droppableId,
+			spellId,
+		});
+	};
+
 	return (
 		<div>
 			<h3>{battle?.title}</h3>
-			<ul className="mb-5 d-flex align-items-center">
-				{Object.values(enemies).map((enemy) => {
-					return (
-						<li key={enemy.id} className="px-4">
-							<p>{enemy.title}</p>
-							<p>health: {enemy.health}</p>
-						</li>
-					);
-				})}
-			</ul>
 
-			<ul className="mb-5 d-flex align-items-center">
-				{player?.spells.map((spell) => (
-					<li key={spell.id}>
-						<button
-							onClick={() => {
-								handleCastSpell({ casterId: player.id, targetId: '', spellId: spell.spellId });
-							}}
-						>
-							{spell.title}
-						</button>
-					</li>
-				))}
-			</ul>
+			<DragDropContext onDragEnd={handleDragEnd}>
+				<>
+					<div className="d-flex">
+						{Object.values(enemies).map((enemy) => {
+							return (
+								<Droppable droppableId={enemy.id}>
+									{(provided) => (
+										<div
+											key={enemy.id}
+											{...provided.droppableProps}
+											ref={provided.innerRef}
+											className="border"
+										>
+											<p>{enemy.title}</p>
+											<p>health: {enemy.health}</p>
+										</div>
+									)}
+								</Droppable>
+							);
+						})}
+					</div>
+
+					<div className="d-flex">
+						{player?.spells.map((spell, spellIndex) => (
+							<Droppable droppableId="SPELLS">
+								{(droppableProvided) => (
+									<div
+										key={spell.id}
+										{...droppableProvided.droppableProps}
+										ref={droppableProvided.innerRef}
+									>
+										<Draggable key={spell.id} draggableId={spell.id} index={spellIndex}>
+											{(draggableProvided) => (
+												<div
+													ref={draggableProvided.innerRef}
+													{...draggableProvided.draggableProps}
+													{...draggableProvided.dragHandleProps}
+													className="border"
+													onClick={() => {
+														handleCastSpell({
+															casterId: player.id,
+															targetId: '',
+															spellId: spell.spellId,
+														});
+													}}
+												>
+													{spell.title}
+												</div>
+											)}
+										</Draggable>
+										{droppableProvided.placeholder}
+									</div>
+								)}
+							</Droppable>
+						))}
+					</div>
+				</>
+			</DragDropContext>
 
 			<div className="border bg-gray200 rounded">
 				<h3>Combat Log</h3>
